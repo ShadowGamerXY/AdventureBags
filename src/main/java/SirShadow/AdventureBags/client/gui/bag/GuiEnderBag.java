95669b0c3059a6889704a53f46d4ccbd18a00bbf
SirShadow <shadowgamerxy@gmail.com>
2016-09-04 09:45:03 +0200

    major bug fix

--- src/main/java/SirShadow/AdventureBags/client/gui/bag/GuiEnderBag.java
+++ src/main/java/SirShadow/AdventureBags/client/gui/bag/GuiEnderBag.java
@@ -1,23 +1,33 @@
 package SirShadow.AdventureBags.client.gui.bag;
 
-import SirShadow.AdventureBags.client.gui.GuiBase;
 import SirShadow.AdventureBags.client.inventory.ender.bag.ContainerEnderBag;
 import SirShadow.AdventureBags.client.inventory.ender.bag.InventoryEnderBag;
 import SirShadow.AdventureBags.lib.LibMain;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
 /**
  * Created by SirShadow on 21. 07. 2016.
  */
-public class GuiEnderBag extends GuiBase
+public class GuiEnderBag extends GuiContainer
 {
 
     private static final ResourceLocation texture = new ResourceLocation(LibMain.ModInfo.MOD_ID,"textures/gui/guiBasicBag.png");
 
     public GuiEnderBag(EntityPlayer player, InventoryEnderBag inventoryBag) {
-        super("Adventure Bag", new ContainerEnderBag(player,inventoryBag), texture);
-        setGuiWidth(175);
-        setGuiHeight(169);
+        super(new ContainerEnderBag(player,inventoryBag));
+        xSize = (176);
+        ySize = (170);
+    }
+
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
+    {
+        mc.renderEngine.bindTexture(texture);
+        int xStart = (width  - 176) / 2;
+        int yStart = (height - 170) / 2;
+        drawTexturedModalRect(xStart,yStart,0,0,xSize,ySize);
     }
 }
--- src/main/java/SirShadow/AdventureBags/client/gui/crate/GuiEnderCrate.java
+++ src/main/java/SirShadow/AdventureBags/client/gui/crate/GuiEnderCrate.java
@@ -1,28 +1,27 @@
 package SirShadow.AdventureBags.client.gui.crate;
 
-import SirShadow.AdventureBags.client.gui.GuiBase;
 import SirShadow.AdventureBags.client.inventory.ender.backpack.InventoryEnderBackapck;
 import SirShadow.AdventureBags.client.inventory.ender.crate.ContainerEnderCrate;
 import SirShadow.AdventureBags.lib.LibMain;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
 /**
  * Created by SirShadow for the mod Roots on 1.8.2016.
  */
-public class GuiEnderCrate extends GuiBase
+public class GuiEnderCrate extends GuiContainer
 {
     private static final ResourceLocation texture = new ResourceLocation(LibMain.ModInfo.MOD_ID,"textures/gui/guiBasicBackpack.png");
 
     public GuiEnderCrate(EntityPlayer player, InventoryEnderBackapck inventoryBag) {
-        super("", new ContainerEnderCrate(player,inventoryBag), null);
-        xSize = 175;
-        ySize = 196;
+        super(new ContainerEnderCrate(player,inventoryBag));
+        xSize = 176;
+        ySize = 197;
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int rawMouseX, int rawMouseY) {
-        super.drawGuiContainerBackgroundLayer(partialTicks, rawMouseX, rawMouseY);
         this.mc.getTextureManager().bindTexture(texture);
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
