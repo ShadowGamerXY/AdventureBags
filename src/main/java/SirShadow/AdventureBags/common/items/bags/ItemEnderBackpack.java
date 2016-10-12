bb593da492397cf4295c793441fe052280e75b0a
SirShadow <shadowgamerxy@gmail.com>
2016-10-12 13:35:44 +0200

    0.4 cleanup

--- src/main/java/SirShadow/AdventureBags/common/items/bags/ItemEnderBackpack.java
+++ src/main/java/SirShadow/AdventureBags/common/items/bags/ItemEnderBackpack.java
@@ -6,32 +6,22 @@ import SirShadow.AdventureBags.common.utils.Util;
 import SirShadow.AdventureBags.common.utils.handler.ConfigurationHandler;
 import SirShadow.AdventureBags.common.utils.helper.TextHelper;
 import SirShadow.AdventureBags.lib.LibMain;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by SirShadow for the mod Roots on 27.7.2016.
  */
 public class ItemEnderBackpack extends ItemBaseAB
 {
-
-    int x,y,z;
-
-    boolean shouldShow = false;
-
     public ItemEnderBackpack() {
         super(LibMain.LibNames.ender_backpack);
     }
@@ -55,41 +45,7 @@ public class ItemEnderBackpack extends ItemBaseAB
             {
                 return;
             }
-            if (shouldShow)
-            {
-                tooltip.add("Saved position:");
-                tooltip.add("X:" + Integer.toString(x) + " Y:" + Integer.toString(y) + " Z:" + Integer.toString(z));
-            }
         }
-    }
-
-    public void createData(ItemStack stack)
-    {
-        NBTTagCompound tag = stack.getTagCompound();
-        if(tag == null)
-        {
-            tag = new NBTTagCompound();
-            stack.setTagCompound(tag);
-        }
-
-        tag.setBoolean("show",shouldShow);
-        tag.setInteger("x",x);
-        tag.setInteger("y",y);
-        tag.setInteger("z",z);
-    }
-
-    public void readData(ItemStack stack)
-    {
-        NBTTagCompound tag = stack.getTagCompound();
-        if(tag == null)
-        {
-            tag = new NBTTagCompound();
-            stack.setTagCompound(tag);
-        }
-        tag.getBoolean("show");
-        tag.getInteger("x");
-        tag.getInteger("y");
-        tag.getInteger("z");
     }
 
     @Override
@@ -109,18 +65,6 @@ public class ItemEnderBackpack extends ItemBaseAB
                     return new ActionResult<>(EnumActionResult.SUCCESS, itemStackIn);
                 }
             }
-            if (ConfigurationHandler.waypoint_marker)
-            {
-                if (playerIn.isSneaking())
-                {
-                    x = (int) playerIn.posX;
-                    y = (int) playerIn.posY;
-                    z = (int) playerIn.posZ;
-
-                    shouldShow = true;
-                }
-            }
-            return new ActionResult<>(EnumActionResult.FAIL, itemStackIn);
         }
         return new ActionResult<>(EnumActionResult.PASS, itemStackIn);
     }
--- src/main/java/SirShadow/AdventureBags/common/items/bags/itemEnderBag.java
+++ src/main/java/SirShadow/AdventureBags/common/items/bags/itemEnderBag.java
@@ -6,17 +6,12 @@ import SirShadow.AdventureBags.common.utils.Util;
 import SirShadow.AdventureBags.common.utils.handler.ConfigurationHandler;
 import SirShadow.AdventureBags.common.utils.helper.TextHelper;
 import SirShadow.AdventureBags.lib.LibMain;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 
@@ -27,11 +22,6 @@ import java.util.List;
  */
 public class itemEnderBag extends ItemBaseAB
 {
-
-    int x,y,z;
-
-    boolean shouldShow;
-
     public itemEnderBag()
     {
         super(LibMain.LibNames.ender_bag);
@@ -57,41 +47,7 @@ public class itemEnderBag extends ItemBaseAB
             {
                 return;
             }
-            if (shouldShow)
-            {
-                tooltip.add("Saved position:");
-                tooltip.add("X:" + Integer.toString(x) + " Y:" + Integer.toString(y) + " Z:" + Integer.toString(z));
-            }
         }
-    }
-
-    public void createData(ItemStack stack)
-    {
-        NBTTagCompound tag = stack.getTagCompound();
-        if(tag == null)
-        {
-            tag = new NBTTagCompound();
-            stack.setTagCompound(tag);
-        }
-
-        tag.setBoolean("show",shouldShow);
-        tag.setInteger("x",x);
-        tag.setInteger("y",y);
-        tag.setInteger("z",z);
-    }
-
-    public void readData(ItemStack stack)
-    {
-        NBTTagCompound tag = stack.getTagCompound();
-        if(tag == null)
-        {
-            tag = new NBTTagCompound();
-            stack.setTagCompound(tag);
-        }
-        tag.getBoolean("show");
-        tag.getInteger("x");
-        tag.getInteger("y");
-        tag.getInteger("z");
     }
 
     @Override
@@ -111,19 +67,6 @@ public class itemEnderBag extends ItemBaseAB
                     return new ActionResult<>(EnumActionResult.SUCCESS, itemStackIn);
                 }
             }
-
-            if (ConfigurationHandler.waypoint_marker)
-            {
-                if (playerIn.isSneaking())
-                {
-                    x = (int) playerIn.posX;
-                    y = (int) playerIn.posY;
-                    z = (int) playerIn.posZ;
-
-                    shouldShow = true;
-                }
-            }
-            return new ActionResult<>(EnumActionResult.FAIL,itemStackIn);
         }
             return new ActionResult<>(EnumActionResult.PASS, itemStackIn);
     }
